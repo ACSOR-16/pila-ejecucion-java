eec12005d77cf36c2cb6706aeb2413728730f4b9
oscar <oscar.aguirre.tarazona@gmail.com>
2023-03-14 19:36:28 -0500

    analisando excepciones

--- src/Flujo.java
+++ src/Flujo.java
@@ -14,6 +14,25 @@ public class Flujo {
         System.out.println("Inicio de metodo2");
         for(int i =1; i<= 5; i++){
         System.out.println("i");
+        try {//intenta error
+            // int numero = 0;
+            // int resultado = i/numero;
+            // System.out.println(resultado);
+            String test = null;
+            System.out.println(test.toString());
+
+        } catch(ArithmeticException exception) {//atrapa error
+
+            System.out.println("Atrapa Arihmetic");
+            System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
+        catch(NullPointerException exception) {//atrapa error
+            
+            System.out.println("Atrapa Nullpointer");
+            System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
         }
         System.out.println("Fin de metodo2");
     }
